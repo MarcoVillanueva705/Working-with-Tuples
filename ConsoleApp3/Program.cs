c39ba5c8fbf6d9a0a7d41fd27d8cf6641fa373b2
marco villanueva <villanuevamarco98@gmail.com>
2020-07-08 20:36:51 -0600

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Tuple<String, int, bool> myTuple = new Tuple<string, int, bool>("yikes", 5, false);
+
+            var myTuple = Tuple.Create(5, "shucks", true);
+            Console.WriteLine(myTuple.Item1);
+        }
+    }
+}
